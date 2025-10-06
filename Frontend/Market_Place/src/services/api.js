5c595b4ca6630a631fe209f40464cc548853c0f9
matiasgf21 <matiasgf21@gmail.com>
2025-10-06 01:30:53 -0300

    arreglo en api.js

--- Frontend/Market_Place/src/services/api.js
+++ Frontend/Market_Place/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:9090/api";
+const BASE_URL = "https://backendmarketplace-h8yv.onrender.com";
 const PRODUCTOS_URL = `${BASE_URL}/productos`;
 
 /**
